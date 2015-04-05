c482af463ee2b9c6e87ff73eb1e63a8dec2367c6
teogorghin <teogorghin@gmail.com>
2015-04-05 14:25:08 +0300

    Update and rename LICENSE to index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+   <!-- HTML meta refresh URL redirection -->
+   <meta http-equiv="refresh"
+   content="0; url=http://pronirax.com">
+</head>
+<body>
+   <p>The page has moved to:
+   <a href="http://www.mydomain.com/new-page.html">this page</a></p>
+</body>
+</html>
